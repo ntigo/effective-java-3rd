af1da20ca52a5ed9eb005cdf283cd7bc02721e16
xo4813 <xo4813@naver.com>
2022-12-13 16:02:20 +0900

    Delete src/main/java/com/coopnc/effectivejava3rd/item03 directory

--- src/main/java/com/coopnc/effectivejava3rd/item03/base/App.java
+++
@@ -1,9 +0,0 @@
-package com.coopnc.effectivejava3rd.item03.base;
-
-public class App {
-    public static void main(String[] args){
-        Singleton singleton = Singleton.getInstance();
-        System.out.println(singleton == Singleton.getInstance());
-    }
-
-}
--- src/main/java/com/coopnc/effectivejava3rd/item03/staticInner/App.java
+++
@@ -1,9 +0,0 @@
-package com.coopnc.effectivejava3rd.item03.staticInner;
-
-public class App {
-
-    public static void main(){
-        Singleton singleton = Singleton.getInstance();
-        System.out.println(singleton);
-    }
-}
