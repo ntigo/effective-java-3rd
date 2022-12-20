616b269784aff4f7861b278c73f31626e5fd8991
xo4813 <xo4813@naver.com>
2022-12-20 21:37:29 +0900

    Update Basic.java

--- src/main/java/com/coopnc/effectivejava3rd/item03/exam/Basic.java
+++ src/main/java/com/coopnc/effectivejava3rd/item03/exam/Basic.java
@@ -1,35 +1,16 @@
 package com.coopnc.effectivejava3rd.item03;
 
-import java.io.*;
-import java.lang.reflect.Constructor;
-import java.lang.reflect.InvocationTargetException;
+public class Basic {
+	public static Basic basic = new Basic();
 
-public class SingletonTest{
+	private  Basic(){}
 
-	public static void doTest() throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException, IOException, ClassNotFoundException {
-		Factory singleton = Factory.getInstance();
-		Factory singleton2 = null;
-		try(ObjectOutput out = new ObjectOutputStream(new FileOutputStream("singleton.obj"))){
-			out.writeObject(singleton);
-		} catch (IOException e) {
-			throw new RuntimeException(e);
-		}
-
-		try (ObjectInput in = new ObjectInputStream(new FileInputStream("singleton.obj"))){
-			singleton2 = (Factory) in.readObject();
-		}
-
-		System.out.printf("deserialization 결과: %s", singleton == singleton2);
-
-
-		Constructor<Factory> constructor = null;
-		constructor = Factory.class.getDeclaredConstructor();
-		constructor.setAccessible(true);
-
-		singleton2 = constructor.newInstance();
-
-		System.out.printf("리플렉션 결과: %s ", singleton == singleton2);
-
+	public static Basic getInstance(){
+//		if (instance == null){
+//			instance = new Basic();
+//		}
+//		return instance;
+		return basic;
 	}
 
 
