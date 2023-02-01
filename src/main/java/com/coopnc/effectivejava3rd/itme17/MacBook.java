637a43e3b76add0ae9c3d00c049fb96360c87039
seoTaeHan <>
2023-02-01 13:55:40 +0900

    아이템 17

---
+++ src/main/java/com/coopnc/effectivejava3rd/itme17/MacBook.java
@@ -0,0 +1,12 @@
+package com.coopnc.effectivejava3rd.itme17;
+
+public class MacBook  extends NoteBook{
+    public MacBook(String name, int weight, int number) {
+        super(name, weight, number);
+    }
+
+    public  void setName(){
+        this.name = "TEST";
+    }
+
+}
---
+++ src/main/java/com/coopnc/effectivejava3rd/itme17/NoteBook.java
@@ -0,0 +1,16 @@
+package com.coopnc.effectivejava3rd.itme17;
+
+public  class NoteBook {
+     String name;
+     int weight;
+
+     int number;
+
+    public NoteBook(String name, int weight, int number) {
+        this.name = name;
+        this.weight = weight;
+        this.number = number;
+    }
+// 커맨드 N 으로 setter 생성
+
+}
