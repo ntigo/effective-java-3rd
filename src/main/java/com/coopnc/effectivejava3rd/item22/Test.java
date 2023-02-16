3f833e85feb23176ccebc630bb2b3fab4a551877
bonghyung <knight9900@naver.com>
2023-02-16 13:21:07 +0900

    Add files via upload

--- src/main/java/com/coopnc/effectivejava3rd/item22/Test.java
+++ src/main/java/com/coopnc/effectivejava3rd/item22/Test.java
@@ -5,15 +5,15 @@ import java.io.ObjectStreamConstants;
 import static com.coopnc.effectivejava3rd.item22.PhysicalConstants.AVOGADROS_NUMBER;
 
 public class Test implements TestInterface {
-	//public static final String NAME = "test";
-	public static void main(String[] args) {
-		System.out.println(NAME);
-	}
+//	public static final String NAME = "test";
+//	public static void main(String[] args) {
+//		System.out.println(NAME);
+//	}
 
 
-	/*double atoms(double mols) {
+	double atoms(double mols) {
 		return AVOGADROS_NUMBER * mols;
-	}*/
+	}
 	//Integer
 	//ObjectStreamConstants
 }
