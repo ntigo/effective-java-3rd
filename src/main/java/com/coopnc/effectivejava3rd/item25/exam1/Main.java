9830a8536d19fb802287f79754774e9b19a1aeb9
seoTaeHan <>
2023-02-28 13:21:49 +0900

    아이템25 추가

---
+++ src/main/java/com/coopnc/effectivejava3rd/item25/exam1/Main.java
@@ -0,0 +1,7 @@
+package com.coopnc.effectivejava3rd.item25.exam1;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(Utensil.NAME + Dessert.NAME);
+    }
+}
---
+++ src/main/java/com/coopnc/effectivejava3rd/item25/exam1/Utensil.java
@@ -0,0 +1,9 @@
+package com.coopnc.effectivejava3rd.item25.exam1;
+
+//class Utensil {
+//    static final String NAME = "pan";
+//}
+//
+//class Dessert {
+//    static final String NAME = "cake";
+//}
