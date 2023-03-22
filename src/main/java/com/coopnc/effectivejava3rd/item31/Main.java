666dc848888fba9bd789c1dcb25e86b8664f5417
xo4813 <xo4813@naver.com>
2023-03-22 16:53:59 +0900

    Update Main.java

--- src/main/java/com/coopnc/effectivejava3rd/item31/Main.java
+++ src/main/java/com/coopnc/effectivejava3rd/item31/Main.java
@@ -8,11 +8,11 @@ public class Main {
         Stack<Number> numberStack = new Stack<>();
         Iterable<Integer> integers = null;
         // Interger는 Number의 하위 타입이니 논리적으로 잘 동작된다
-        numberStack.pushAll(integers);
+        //numberStack.pushAll(integers);
 
         Collection<Object> numberCollection = new ArrayList<>();
         // Number는 Object의 하위 타입이니 논리적으로 잘 동작된다
-        numberStack.popAll(numberCollection);
+        //numberStack.popAll(numberCollection);
 
     }
 }
